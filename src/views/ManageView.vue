0764877bb406a6edf7601af76f6113253f91d428
28H2O2 <hcy1111hcy@sina.com>
2023-04-02 20:59:52 +0800

    ✨ feat(H2O2): 在处理完请求后，按钮被disabled

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -31,8 +31,8 @@
           >
             同意
           </el-button> -->
-          <el-button type="success" size="small" @click="approveShop(row)">同意</el-button>
-          <el-button type="danger" size="small" @click="rejectShop(row)">拒绝</el-button>
+          <el-button type="success" size="small" @click="approveShop(row)" :disabled="isButtonDisabled(row)">同意</el-button>
+          <el-button type="danger" size="small" @click="rejectShop(row)" :disabled="isButtonDisabled(row)">拒绝</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -56,6 +56,19 @@ export default {
       userName: ''
     }
   },
+  // NOTE: 用computed来实现按钮的disabled属性，如果不是待审核状态，就禁用按钮
+  // NOTE: 需要注意的是，这里的row是一个参数，要在调用的时候传入
+  computed: {
+  isButtonDisabled() {
+    return (row) => {
+      if (row.status !== '待审核') {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  },
+},
   methods: {
     fetchData: async function () {
       try {
