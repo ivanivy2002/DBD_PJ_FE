c19031299da04db94187c6d12a8ce44caf0c22a0
28H2O2 <hcy1111hcy@sina.com>
2023-04-07 09:58:24 +0800

    🌈 style(H2O2): 暗色主体的尝试
    element-plus官网的办法http://element-plus.org/zh-CN/guide/dark-mode.html#%E8%87%AA%E5%AE%9A%E4%B9%89%E5%8F%98%E9%87%8F

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+// BUG: 暗色主题
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/css/dark/css-vars.css'
+
 const app = createApp(App)
 
 app.use(ElementPlus)
