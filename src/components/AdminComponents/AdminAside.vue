436950a0b68b3e2bf83cfda8f47fe168f23a365f
Ivan <21302010040@m.fudan.edu.cn>
2023-04-16 13:56:03 +0800

    feat(Ivan): managemoney

--- src/components/AdminComponents/AdminAside.vue
+++ src/components/AdminComponents/AdminAside.vue
@@ -1,112 +1,162 @@
 <template>
-  <div class="sidebar-container">
-    <el-menu
-      default-active="1"
-      class="sidebar-menu"
-      :collapse-transition="false"
-      :unique-opened="true"
-      :background-color="'#2e2e2e'"
-      :text-color="'#fff'"
-      :active-text-color="'#ffd04b'"
-    >
-      <el-menu-item index="1" class="sidebar-menu-item" @click="handleClick('account')">
-        <i class="el-icon-user"></i>
-        <span slot="title">商城账户</span>
-      </el-menu-item>
-      <el-menu-item index="2" class="sidebar-menu-item" @click="handleClick('transfer')">
-        <i class="el-icon-document"></i>
-        <span slot="title">转账流水</span>
-      </el-menu-item>
-    </el-menu>
-    <div class="sidebar-content">
-      <transition name="fade" mode="out-in">
-        <div v-if="activeTab === 'account'" key="account" class="account-tab">
-          <p class="account-balance">当前账户余额: {{ accountBalance }}</p>
-          <button class="account-btn">充值</button>
+    <div class="sidebar-container">
+        <el-menu
+                default-active="1"
+                class="sidebar-menu"
+                :collapse-transition="false"
+                :unique-opened="true"
+                :background-color="'#2e2e2e'"
+                :text-color="'#fff'"
+                :active-text-color="'#ffd04b'"
+        >
+            <el-menu-item index="1" class="sidebar-menu-item" @click="handleClick('account')">
+                <i class="el-icon-user"></i>
+                <span slot="title">商城账户</span>
+            </el-menu-item>
+            <el-menu-item index="2" class="sidebar-menu-item" @click="handleClick('transfer')">
+                <i class="el-icon-document"></i>
+                <span slot="title">转账流水</span>
+            </el-menu-item>
+        </el-menu>
+        <div class="sidebar-content">
+            <transition name="fade" mode="out-in">
+                <div v-if="activeTab === 'account'" key="account" class="account-tab">
+                    <div class="account-management">
+                        <h2 class="page-title">账户管理</h2>
+                        <div class="profit-account">
+                            <h3 class="section-title">商城利润账户</h3>
+                            <div class="account-info">
+                                <div class="balance">余额：{{ profitAccount.balance }}</div>
+                                <div>
+                                    <el-form ref="balanceForm" label-width="120px" class="balance-form">
+                                        <el-form-item label="充值金额" prop="balance">
+                                            <el-input v-model="rechargeAmount" type="number"></el-input>
+                                        </el-form-item>
+                                        <el-form-item>
+                                            <el-button type="primary" @click="recharge(rechargeAmount)">充值</el-button>
+                                        </el-form-item>
+                                    </el-form>
+                                </div>
+                                <!--                <el-button type="primary" @click="showRechargeDialog = true">充值</el-button>-->
+                            </div>
+                        </div>
+                        <div class="middle-account">
+                            <h3 class="section-title">商城中间账户</h3>
+                            <div class="account-info">
+                                <div class="balance">余额：{{ middleAccount.balance }}</div>
+                            </div>
+                        </div>
+                        <el-dialog title="充值商城利润账户" v-model="showRechargeDialog">
+                            <el-form :model="rechargeForm" label-width="80px">
+                                <el-form-item label="充值金额">
+                                    <el-input v-model.number="rechargeForm.amount"></el-input>
+                                </el-form-item>
+                            </el-form>
+                            <div slot="footer" class="dialog-footer">
+                                <el-button @click="showRechargeDialog = false">取消</el-button>
+                                <el-button type="primary" @click="recharge">确认充值</el-button>
+                            </div>
+                        </el-dialog>
+                    </div>
+                </div>
+                <div v-else-if="activeTab === 'transfer'" key="transfer" class="transfer-tab">
+                    <ul class="transfer-list">
+                        <li v-for="(item, index) in transferList" :key="index">
+                            <span>{{ item.date }}</span>
+                            <span>{{ item.description }}</span>
+                            <span>{{ item.amount }}</span>
+                        </li>
+                    </ul>
+                    <button class="transfer-btn">新建转账</button>
+                </div>
+            </transition>
         </div>
-        <div v-else-if="activeTab === 'transfer'" key="transfer" class="transfer-tab">
-          <ul class="transfer-list">
-            <li v-for="(item, index) in transferList" :key="index">
-              <span>{{ item.date }}</span>
-              <span>{{ item.description }}</span>
-              <span>{{ item.amount }}</span>
-            </li>
-          </ul>
-          <button class="transfer-btn">新建转账</button>
-        </div>
-      </transition>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'AdminAside',
-  // components: { AdminAside },
-  data() {
-    return {
-      activeTab: 'account',
-      accountBalance: 1000,
-      transferList: [
-        { date: '2023-04-01', description: '购买商品', amount: '-100' },
-        { date: '2023-03-31', description: '转账', amount: '+200' },
-        { date: '2023-03-30', description: '充值', amount: '+500' }
-      ]
+    name: 'AdminAside',
+    data() {
+        return {
+            activeTab: 'account',
+            accountBalance: 1000,
+            transferList: [
+                {date: '2023-04-01', description: '购买商品', amount: '-100'},
+                {date: '2023-03-31', description: '转账', amount: '+200'},
+                {date: '2023-03-30', description: '充值', amount: '+500'}
+            ],
+            profitAccount: {
+                balance: 10000,
+            },
+            middleAccount: {
+                balance: 99999,
+            },
+            merchantAccounts: [
+                {name: '商户1', balance: 5000},
+                {name: '商户2', balance: 3000},
+                {name: '商户3', balance: 2000}
+            ],
+            showRechargeDialog: false,
+            rechargeForm: {
+                amount: 0
+            },
+            rechargeAmount: 0,
+        }
+    },
+    methods: {
+        handleClick(tab) {
+            this.activeTab = tab
+        }
     }
-  },
-  methods: {
-    handleClick(tab) {
-      this.activeTab = tab
-    }
-  }
 }
 </script>
 
 <style scoped>
 .sidebar-container {
-  display: flex;
-  flex-direction: row;
-  height: 100%;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
 }
 
 .sidebar-menu {
-  width: 80px;
+    width: 80px;
 }
 
 .sidebar-menu-item {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 .sidebar-content {
-  flex: 1;
-  padding: 20px;
+    flex: 1;
+    padding: 20px;
 }
 
 .account-tab,
 .transfer-tab {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 .account-balance {
-  font-size: 24px;
-  margin-bottom: 20px;
+    font-size: 24px;
+    margin-bottom: 20px;
 }
 
 .account-btn,
 .transfer-btn {
-  margin-top: 20px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  color: #fff;
-  background-color: #ffd04b;
-  cursor: pointer;
+    margin-top: 20px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #ffd04b;
+    cursor: pointer;
 }
 </style>
--- src/views/Admin/ManageMoneyView.vue
+++ src/views/Admin/ManageMoneyView.vue
@@ -1,155 +1,214 @@
 <template>
-  <div class="account-management">
-    <h2 class="page-title">账户管理</h2>
-    <div class="profit-account">
-      <h3 class="section-title">商城利润账户</h3>
-      <div class="account-info">
-        <div class="balance">余额：{{ profitAccount.balance }}</div>
-        <el-form ref="balanceForm" label-width="120px" class="balance-form">
-          <el-form-item label="充值金额" prop="balance">
-            <el-input v-model="rechargeAmount" type="number"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="recharge(rechargeAmount)">充值</el-button>
-          </el-form-item>
-        </el-form>
-        <!--                <el-button type="primary" @click="showRechargeDialog = true">充值</el-button>-->
-      </div>
+<!--    <AdminAside/>-->
+    <div class="account-management">
+        <h2 class="page-title">账户管理</h2>
+        <div class="profit-account">
+            <h3 class="section-title">商城利润账户</h3>
+            <div class="account-info">
+                <div class="balance">余额：{{ profitAccount.balance }}</div>
+                <div>
+                    <el-form ref="balanceForm" label-width="120px" class="balance-form">
+                        <el-form-item label="充值金额" prop="balance">
+                            <el-input v-model="rechargeAmount" type="number"></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="recharge(rechargeAmount)">充值</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+                <!--                <el-button type="primary" @click="showRechargeDialog = true">充值</el-button>-->
+            </div>
+        </div>
+        <div class="middle-account">
+            <h3 class="section-title">商城中间账户</h3>
+            <div class="account-info">
+                <div class="balance">余额：{{ middleAccount.balance }}</div>
+            </div>
+        </div>
+        <el-dialog title="充值商城利润账户" v-model="showRechargeDialog">
+            <el-form :model="rechargeForm" label-width="80px">
+                <el-form-item label="充值金额">
+                    <el-input v-model.number="rechargeForm.amount"></el-input>
+                </el-form-item>
+            </el-form>
+            <div  class="dialog-footer">
+                <el-button @click="showRechargeDialog = false">取消</el-button>
+                <el-button type="primary" @click="recharge">确认充值</el-button>
+            </div>
+        </el-dialog>
     </div>
-    <div class="merchant-accounts">
-      <h3 class="section-title">商城中间账户</h3>
-      <el-table :data="merchantAccounts">
-        <el-table-column prop="name" label="名称"></el-table-column>
-        <el-table-column prop="balance" label="余额"></el-table-column>
-      </el-table>
-    </div>
-    <el-dialog title="充值商城利润账户" v-model="showRechargeDialog">
-      <el-form :model="rechargeForm" label-width="80px">
-        <el-form-item label="充值金额">
-          <el-input v-model.number="rechargeForm.amount"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="showRechargeDialog = false">取消</el-button>
-        <el-button type="primary" @click="recharge">确认充值</el-button>
-      </div>
-    </el-dialog>
-  </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { ElMessage } from 'element-plus'
-
+import {ElMessage} from 'element-plus'
+import AdminAside from "@/components/AdminComponents/AdminAside.vue";
 export default {
-  data() {
-    return {
-      profitAccount: {
-        balance: 10000
-      },
-      merchantAccounts: [
-        { name: '商户1', balance: 5000 },
-        { name: '商户2', balance: 3000 },
-        { name: '商户3', balance: 2000 }
-      ],
-      showRechargeDialog: false,
-      rechargeForm: {
-        amount: 0
-      },
-      rechargeAmount: 0
-    }
-  },
-  methods: {
-    // recharge() {
-    //   this.profitAccount.balance += this.rechargeForm.amount
-    //
-    //   this.profitAccount.transactions.push({
-    //     date: new Date().toISOString().substr(0, 10),
-    //     type: '充值',
-    //     amount: this.rechargeForm.amount
-    //   })
-    //   this.rechargeForm.amount = 0
-    //   // this.showRechargeDialog
-    // },
-    recharge(rechargeAmount) {
-      console.log('rechargeAmount before try ' + this.rechargeAmount)
-      try {
-        // this.shopId = localStorage.getItem('shopId')
-        // eslint-disable-next-line no-unused-vars
-        const response = axios
-          .post('http://localhost:9000/admin/recharge', null, {
-            params: {
-              amount: rechargeAmount
-              // TODO: 这里amount和balance的命名和关系
+    components:{
+        AdminAside
+    },
+    data() {
+        return {
+            profitAccount: {
+                balance: 10000,
+            },
+            middleAccount: {
+                balance: 99999,
+            },
+            merchantAccounts: [
+                {name: '商户1', balance: 5000},
+                {name: '商户2', balance: 3000},
+                {name: '商户3', balance: 2000}
+            ],
+            showRechargeDialog: false,
+            rechargeForm: {
+                amount: 0
+            },
+            rechargeAmount: 0,
+        }
+    },
+    methods: {
+        // recharge() {
+        //   this.profitAccount.balance += this.rechargeForm.amount
+        //
+        //   this.profitAccount.transactions.push({
+        //     date: new Date().toISOString().substr(0, 10),
+        //     type: '充值',
+        //     amount: this.rechargeForm.amount
+        //   })
+        //   this.rechargeForm.amount = 0
+        //   // this.showRechargeDialog
+        // },
+        recharge(rechargeAmount) {
+            console.log('rechargeAmount before try ' + this.rechargeAmount)
+            try {
+                // this.shopId = localStorage.getItem('shopId')
+                // eslint-disable-next-line no-unused-vars
+                const response = axios
+                    .post('http://localhost:9000/admin/recharge', null, {
+                        params: {
+                            amount: rechargeAmount
+                            // TODO: 这里amount和balance的命名和关系
+                        }
+                    })
+                    // eslint-disable-next-line no-unused-vars
+                    .then((response) => {
+                        // console.log(response)
+                        console.log('rechargeAmount after post' + this.rechargeAmount)
+                        // this.balance = response.data.balance
+                        ElMessage({
+                            showClose: true,
+                            type: 'success', //如果失败,未连接上后端
+                            message: '充值成功'
+                        })
+                        this.getBalance() //刷新余额
+                    })
+            } catch (error) {
+                console.log(error)
+                ElMessage({
+                    showClose: true,
+                    type: 'error', //如果失败,未连接上后端
+                    message: '充值失败, 开发问题'
+                })
             }
-          })
-          // eslint-disable-next-line no-unused-vars
-          .then((response) => {
-            // console.log(response)
-            console.log('rechargeAmount after post' + this.rechargeAmount)
-            // this.balance = response.data.balance
-            ElMessage({
-              showClose: true,
-              type: 'success', //如果失败,未连接上后端
-              message: '充值成功'
-            })
-            this.getBalance() //刷新余额
-          })
-      } catch (error) {
-        console.log(error)
-        ElMessage({
-          showClose: true,
-          type: 'error', //如果失败,未连接上后端
-          message: '充值失败, 开发问题'
-        })
-      }
-    }
-  }
+        },
+        async getBalance() {
+            // 发送请求获取余额
+            try {
+                // eslint-disable-next-line no-unused-vars
+                const response = await axios
+                    .get('http://localhost:9000/admin/displayProfitAccount')
+                    .then((response) => {
+                        this.profitAccount.balance = response.data.data.balance
+                        console.log('profitAccount.balance ' + this.profitAccount.balance)
+                    })
+            } catch (error) {
+                console.log(error)
+                ElMessage({
+                    showClose: true,
+                    type: 'error', //如果失败,未连接上后端
+                    message: '修改信息失败, 开发问题'
+                })
+            }
+            ;
+            try {
+                // eslint-disable-next-line no-unused-vars
+                const response = await axios
+                    .get('http://localhost:9000/admin/displayMiddleAccount')
+                    .then((response) => {
+                        this.middleAccount.balance = response.data.data.balance
+                        console.log('middleAccount.balance ' + this.middleAccount.balance)
+                    })
+            } catch (error) {
+                console.log(error)
+                ElMessage({
+                    showClose: true,
+                    type: 'error', //如果失败,未连接上后端
+                    message: '修改信息失败, 开发问题'
+                })
+            }
+            ;
+
+            // // 前端写死的假数据
+            // this.balance = 100.0
+        },
+    },
+    mounted() {
+        // 获取信息和余额
+        this.getBalance();
+    },
 }
 </script>
 
 <style scoped>
 .account-management {
-  margin: 20px;
-  padding: 20px;
-  background-color: #f0f0f0;
-  border-radius: 10px;
+    margin: 20px;
+    padding: 20px;
+    background-color: #f0f0f0;
+    border-radius: 10px;
 }
 
 .page-title {
-  color: #1e90ff;
+    color: #1e90ff;
 }
 
 .section-title {
-  margin-top: 20px;
-  margin-bottom: 10px;
-  color: #1e90ff;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    color: #1e90ff;
 }
 
 .account-info {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
+    /*display: flex;*/
+    align-items: center;
+    margin-bottom: 10px;
+    width: 30%;
 }
 
 .balance {
-  margin-right: 20px;
+    /*border-color: #2d2d2d;*/
+    margin-left: 0px;
+    /*margin-right: 20px;*/
+}
+
+.balance-form {
+    margin-top: 20px;
 }
 
 .transaction-list {
-  max-height: 300px;
-  overflow: auto;
+    max-height: 300px;
+    overflow: auto;
 }
 
 .el-dialog__body {
-  padding: 20px;
+    padding: 20px;
 }
 
 .el-table__body {
-  background-color: #fff;
+    background-color: #fff;
 }
 
 .el-table__row--striped:nth-child(even) {
-  background-color: #f5f5f5;
+    background-color: #f5f5f5;
 }
 </style>
