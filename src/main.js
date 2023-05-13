cbfed781a0da099aecf361784a47449dbb87eafd
Ivan <2410297162@qq.com>
2023-05-14 07:49:16 +0800

    build(Ivan): [Global] implement Quasar failed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+// import { Quasar } from 'quasar'
 import App from './App.vue'
 import router from './router'
 
@@ -8,21 +8,34 @@ import './assets/main.css'
 // import './assets/element-ui.scss'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
+// Import icon libraries
+// import '@quasar/extras/material-icons/material-icons.css'
 
 // import 'element-plus/theme-chalk/light/css-vars-light.css'
 // import './assets/css/light/css-vars-light.css'
 // import 'element-plus/theme-chalk/dark/css-vars-dark.css'
 // import 'element-plus/theme-chalk/dark/css-vars.css'
 // import './assets/css/dark/css-vars-dark.css'
+// const myApp = createApp(App)
+// myApp.use(router)
+// myApp.use(Quasar)
+// myApp.use(ElementPlus)
+// myApp.mount('#app')
+// myApp.use(createPinia())
+// myApp.use(Quasar, {
+//   plugins: {} // import Quasar plugins and add here
+// })
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//   myApp.component(key, component)
+// }
+// myApp.mount('#app')
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+// import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // plugins: [vue({
+  //   template: { transformAssetUrls }
+  // }),
+  //   quasar({
+  //     sassVariables: 'src/assets/quasar-variables.sass'
+  //   })
+  // ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
